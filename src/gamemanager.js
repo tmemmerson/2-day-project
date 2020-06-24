d3f2e937e32b4b67026c6e1537187abce0457056
Tristan Emmerson <tristan@stickerslug.com>
2020-06-24 14:57:10 -0700

    add random enemy function and switch on classes.

--- src/gamemanager.js
+++ src/gamemanager.js
@@ -36,5 +36,17 @@ let GameManager = {
     let getEnemy = document.querySelector(".enemy");
     let enemy00 = new Enemy("Rogue", 100,100,100,100,100);
     let enemy01 = new Enemy("Goblin", 75,125,75,125,100);
+    let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
+    switch (chooseRandomEnemy) {
+    case 0:
+      enemy = enemy00;
+      break;
+    case 1:
+      enemy = enemy01;
+      break;
   }
-};
+    getHeader.innerHTML = '<p>Task: Choose your move!</p>';
+    getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+    getEnemy.innerHTML = '<img src="img/' + enemy.enemyType.toLowerCase() + '.png" alt="' + enemy.enemyType + '" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+  }
+}
