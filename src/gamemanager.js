4dc071c7c431fcc7506401cd1d75e7013d7c4e03
Tristan Emmerson <tristan@stickerslug.com>
2020-06-24 15:02:08 -0700

    refactor.

--- src/gamemanager.js
+++ src/gamemanager.js
@@ -47,6 +47,6 @@ let GameManager = {
   }
     getHeader.innerHTML = '<p>Task: Choose your move!</p>';
     getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
-    getEnemy.innerHTML = '<img src="img/' + enemy.enemyType.toLowerCase() + '.png" alt="' + enemy.enemyType + '" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+    getEnemy.innerHTML = '<img src="../img/' + enemy.enemyType.toLowerCase() + '2.jpg" alt="' + enemy.enemyType + '" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
   }
 }
